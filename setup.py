8b464f7e9fd60c04230d1f884f8f5f93509274d8
Paolo Rechia <paolorechia@gmail.com>
2023-04-28 15:22:48 +0200

    Bootstrap

---
+++ setup.py
@@ -0,0 +1,21 @@
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import codecs
+from setuptools import setup
+
+
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return codecs.open(file_path, encoding="utf-8").read()
+
+
+setup(
+    name="gptq_for_llama",
+    version="0.1.0",
+    url="https://github.com/paolorechia/GPTQ-for-LLaMa",
+    src=".",
+    python_requires=">=3.9",
+)
